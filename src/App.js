917c3b0ae9d963995a2183bfa651f84cdfef80e9
Maximiliano Delgado <mdelgado@baufest.com>
2020-11-25 13:19:52 -0300

    edit gh actions

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>or not 4</p>
+        <p>or not 5</p>
         <a
           className="App-link"
           href="https://reactjs.org"
